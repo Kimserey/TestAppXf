76bba4d0b8860dff94396b65183b436ac15ac2c5
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-13 08:37:28 +0100

    Changed style to light dark action bar. Just had to add Theme on main activity.

--- AppAndroidCSharp/MainActivity.cs
+++ AppAndroidCSharp/MainActivity.cs
@@ -6,17 +6,22 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Content.PM;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace AppAndroidCSharp
 {
-    [Activity(Label = "AppAndroidCSharp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    [Activity(Theme = "@android:style/Theme.Material.Light.DarkActionBar", 
+              Label = "AppAndroidCSharp", 
+              Icon = "@drawable/icon", 
+              MainLauncher = true, 
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Forms.Init(this, bundle);
             LoadApplication(new Library.ButtonTest.App());
         }
     }
