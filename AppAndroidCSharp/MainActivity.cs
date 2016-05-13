fcbf0d4558c5de83009ac2255f7c746012b76829
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-13 08:23:23 +0100

    added cs xamarin proj

---
+++ AppAndroidCSharp/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+
+namespace AppAndroidCSharp
+{
+    [Activity(Label = "AppAndroidCSharp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new Library.ButtonTest.App());
+        }
+    }
+}
+
